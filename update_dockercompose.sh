781f969e651e80a7eebe8969f885efee5714878b
fiadanza <francesco.iadanza@gmail.com>
2020-01-27 14:40:28 +0100

    Delete update_dockercompose.sh

--- update_dockercompose.sh
+++
@@ -1,2 +0,0 @@
-sudo sed -i "s/nbi\:\${TAG\:-latest}/nbi\:\${TAG\:-oidc}/" /etc/osm/docker/docker-compose.yaml
-sudo sed -i "s/light-ui\:\${TAG\:-latest}/light-ui\:\${TAG\:-oidc}/" /etc/osm/docker/docker-compose.yaml
